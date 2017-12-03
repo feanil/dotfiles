ec4d4da86e213b7c926228e18817971d85af77df
Feanil Patel <feanil@gmail.com>
2017-12-03 12:58:31 -0500

    Get rid of XMobar now that I'm running with xfce.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,23 +7,12 @@ import Graphics.X11.ExtraTypes.XF86
 import System.IO
 
 main = do
-     spawnPipe "killall trayer; trayer --edge top --align right --width 25 --transparent true --alpha 0 --tint 0 --height 17"
-     spawnPipe "killall nm-applet; nm-applet"
-     spawnPipe "killall xfce4-volumed; xfce4-volumed"
-     spawnPipe "killall xfce4-power-manager; xfce4-power-manager"
-     spawnPipe "killall xscreensaver -nosplash; xscreensaver -nosplash"
      spawnPipe "synclient MaxTapTime=0"
      spawnPipe "synclient TouchpadOff=1"
      spawnPipe "setxkbmap -option 'ctrl:nocaps'"
-     spawnPipe "xloadimage personalize/lotus-wallpaper.jpg -onroot -fullscreen"
-     xmproc <- spawnPipe "/usr/bin/xmobar /home/feanil/.config/xmobar/xmobarrc"
      xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         } `additionalKeys`
 	[
