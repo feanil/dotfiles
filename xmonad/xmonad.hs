186e97a0f0c9ecdc8cd4d0a11e7fdb937d477e5c
Feanil Patel <feanil@gmail.com>
2018-02-19 13:10:40 -0500

    Simplify xmonad config.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,13 +6,11 @@ import XMonad.Hooks.ManageDocks
 import System.IO
 
 main = do
+     spawnPipe "killall xfce4-panel; xfce4-panel;"
      spawnPipe "synclient MaxTapTime=0"
---     spawnPipe "synclient TouchpadOff=1"
      spawnPipe "setxkbmap -option 'ctrl:nocaps'"
      xmonad $ xfceConfig 
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        { modMask = mod4Mask     -- Rebind Mod to the Windows key
         } `additionalKeys`
 	[ ((mod4Mask .|. shiftMask, xK_Return), spawn "xfce4-terminal")
 	, ((mod4Mask , xK_p), spawn "rofi -show run")
