2f69750c4c3ee564d6dfd5bf684d34d847692953
Feanil Patel <feanil@gmail.com>
2021-11-09 21:53:14 -0500

    Modified shell related settings.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,13 +2,12 @@ import XMonad
 import XMonad.Util.Run
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Config.Xfce
-import XMonad.Hooks.ManageDocks
 import System.IO
 
 main = do
-     spawnPipe "sleep 5; xfce4-panel -r;"
+     spawnPipe "sleep 2; xfce4-panel -r;"
 --     spawnPipe "killall xautolock; xautolock -time 5 -locker 'gnome-screensaver-command -l';"
-     spawnPipe "synclient MaxTapTime=0"
+--     spawnPipe "synclient MaxTapTime=0"
      spawnPipe "setxkbmap -option 'ctrl:nocaps'"
      xmonad $ xfceConfig
         { modMask = mod4Mask     -- Rebind Mod to the Windows key
