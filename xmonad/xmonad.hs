690cb62cd284cf1f7b5cafebdc37c53bc35a6753
Feanil Patel <feanil@gmail.com>
2022-02-25 09:46:10 -0500

    Update xmonad config.
    * Turn the screensaver back on and update the lock command.
    * Add a new shortcut for swapping capslock with ctrl.
    * Fix some inconsistent tab and space usage.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,12 +7,13 @@ import System.IO
 main = do
      spawnPipe "sleep 2; xfce4-panel -r;"
      spawnPipe "synclient MaxTapTime=0"
---     spawnPipe "killall xautolock; xautolock -time 5 -locker 'gnome-screensaver-command -l';"
+     spawnPipe "killall xautolock; xautolock -time 5 -locker 'xdg-screensaver lock';"
      spawnPipe "setxkbmap -option 'ctrl:nocaps'"
      xmonad $ xfceConfig
         { modMask = mod4Mask     -- Rebind Mod to the Windows key
         } `additionalKeys`
-	[ ((mod4Mask .|. shiftMask, xK_Return), spawn "xfce4-terminal")
-	, ((mod4Mask , xK_p), spawn "rofi -show run")
-	, ((mod4Mask .|. shiftMask, xK_l), spawn "xdg-screensaver lock")
-	]
+        [ ((mod4Mask .|. shiftMask, xK_Return), spawn "xfce4-terminal")
+        , ((mod4Mask , xK_p), spawn "rofi -show run")
+        , ((mod4Mask .|. shiftMask, xK_l), spawn "xdg-screensaver lock")
+        , ((mod4Mask, xK_c), spawn "setxkbmap -option 'ctrl:nocaps'")
+        ]
