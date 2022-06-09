3610de094f4cfc4c7121db237f4d3b5876ef2543
Feanil Patel <feanil@gmail.com>
2022-06-08 23:30:16 -0400

    feat: Customize layouts to add reflection ability.
    Added 2 new layouts that put the main pane on the right/bottom of the
    screen and added some keyboard shortcuts to easily toggle reflection.
    
    I could have just added the keyboard shortcuts and not add the now
    reflected layouts but deciced to do both since I don't know which way
    I'll prefer to work with them yet.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,6 +4,23 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Config.Xfce
 import System.IO
 
+-- Use these two to put the master pane on the right
+-- https://hackage.haskell.org/package/xmonad-contrib-0.17.0/docs/XMonad-Layout-Reflect.html
+import XMonad.Layout.Reflect
+import XMonad.Layout.MultiToggle
+
+
+myLayouts = desktopLayoutModifiers
+        $ mkToggle (single REFLECTX)
+        $ mkToggle (single REFLECTY)
+        $
+            (reflectHoriz $ Tall 1 0.03 0.5) -- Put the master pane on the right
+        ||| Tall 1 0.03 0.5
+        ||| (reflectVert $ Mirror (Tall 1 0.03 0.5)) -- Put the master pane on the bottom
+        ||| Mirror (Tall 1 0.03 0.5)
+        ||| Full
+
+
 main = do
      spawnPipe "sleep 2; xfce4-panel -r;"
      spawnPipe "synclient MaxTapTime=0"
@@ -11,9 +28,15 @@ main = do
      spawnPipe "setxkbmap -option 'ctrl:nocaps'"
      xmonad $ xfceConfig
         { modMask = mod4Mask     -- Rebind Mod to the Windows key
+        -- , layoutHook = myLayouts -- Change the possible layouts
+        -- , layoutHook = desktopLayoutModifiers $ Mirror (Mirror (Tall 1 0.03 0.5)) -- Change the possible layouts
+        , layoutHook = myLayouts
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_Return), spawn "xfce4-terminal")
         , ((mod4Mask , xK_p), spawn "rofi -show run")
         , ((mod4Mask .|. shiftMask, xK_l), spawn "xdg-screensaver lock")
         , ((mod4Mask, xK_c), spawn "setxkbmap -option 'ctrl:nocaps'")
+        -- Be able to toggle master pane location
+        , ((mod4Mask, xK_r), sendMessage $ Toggle REFLECTX)
+        , ((mod4Mask .|. shiftMask, xK_r), sendMessage $ Toggle REFLECTY)
         ]
